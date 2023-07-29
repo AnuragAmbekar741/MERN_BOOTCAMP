ab5ef26eaabd0f68082813e8cf74fc6cbe910ca7
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2023-07-29 21:18:49 +0530

    TS 2

--- week-8/Advance TS/index.js
+++ week-8/Advance TS/index.js
@@ -1,5 +1,11 @@
+//Way to define return type of a function 
 function getFirstEle(arr) {
     return arr[0];
 }
 var ans = getFirstEle([1, 2, 3]);
 console.log(ans);
+function getFirstEle2(arr) {
+    return arr[0];
+}
+var ans2 = getFirstEle2(['a', 'b', 'c']);
+console.log(ans2);
--- week-8/Advance TS/index.ts
+++ week-8/Advance TS/index.ts
@@ -1,8 +1,62 @@
+//Way to define arr type
 type NumberArr = number[]
 
-function getFirstEle(arr:NumberArr){
+//Way to define return type of a function 
+function getFirstEle(arr:NumberArr):number{
     return arr[0]
 }
 
 let ans = getFirstEle([1,2,3])
 console.log(ans)
+
+
+type input = (number | string)[]
+
+function getFirstEle2(arr:input){
+    return arr[0]
+}
+
+
+
+let ans2 = getFirstEle2(['a','b','c'])
+console.log(ans2)
+// ans2.toUpperCase()
+
+function getFirstEle3<G>(arr:G[]):G{
+    return arr[0]
+}
+
+let ans3 = getFirstEle3([1,3,5,6])
+let ans4 = getFirstEle3(['a','b','c'])
+ans4.toUpperCase()
+
+
+function swap<G>(input1:G,input2:G):[G,G]{
+    return [input2,input1]
+}
+
+let sol = swap(1,2)
+
+function swap2<G1,G2>(input1:G1,input2:G2):[G2,G1]{
+    return [input2,input1]
+}
+
+const swapArw = <T1,T2>(ip1:T1,ip2:T2):[T2,T1]=>{
+    return [ip2,ip1]
+}
+
+let sol2 = swap2(1,'a')
+
+
+interface Todo{
+    title:string
+    desc:string
+    id:number
+    status:boolean
+}
+
+type UpdataTodoInput = Partial<Todo>
+
+function updateTodo(id:number,todo:UpdataTodoInput){
+    
+}
